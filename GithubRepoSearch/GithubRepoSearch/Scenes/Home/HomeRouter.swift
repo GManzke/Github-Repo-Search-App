9e81d9c1d5e373d9f41c764bd5cef177a4c1711e
Gabriel Manzke <gabrielmanzke@Mac-de-Gabriel.local>
2020-05-21 17:46:55 -0700

    Adds second screen on search done with results from API

---
+++ GithubRepoSearch/GithubRepoSearch/Scenes/Home/HomeRouter.swift
@@ -0,0 +1,9 @@
+//
+//  HomeRouter.swift
+//  GithubRepoSearch
+//
+//  Created by Gabriel Manzke on 12/05/20.
+//  Copyright © 2020 Gabriel Manzke. All rights reserved.
+//
+
+import Foundation
---
+++ GithubRepoSearch/GithubRepoSearch/Scenes/Results/View/CustomCell/ResultCell.swift
@@ -0,0 +1,30 @@
+//
+//  ResultCell.swift
+//  GithubRepoSearch
+//
+//  Created by Gabriel Manzke on 21/05/20.
+//  Copyright © 2020 Gabriel Manzke. All rights reserved.
+//
+
+import UIKit
+
+class ResultCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
